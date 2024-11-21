dc7b0a6d44815db150ed03e45a970839a0ccb236
3CViFernanda <00001093616179sp@al.educacao.sp.gov.br>
2024-11-21 10:32:10 -0300

    Add files via upload

---
+++ ferramentas de estudos - fernanda/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Script?',
+    'O Script é conjunto de instruções para que uma função seja executada em determinado aplicativo'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que continente o Brasil está?',
+    'America do Sul'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tempo em Inglês?',
+    'Tempo em ingles é Time (taime)'
+)
